a73587005eb945e7c95130525e0230b91039cc42
Andres Fernando <andres.fernando08@gmail.com>
2021-11-13 17:20:53 -0500

    [add] more tests

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -4,4 +4,15 @@ describe('test sum function', () => {
 	test('adds 1 + 2 must be equal to 3', () => {
 		expect(sum(1, 2)).toBe(3);
 	});
+
+	test('2 + 2 is four', () => {
+		expect(sum(2, 2)).toBe(4);
+	});
+
+	test('object assignment', () => {
+		const data = { one: 1, two: 0 };
+		data.two = 2;
+		expect(data).toEqual({ one: 1, two: 2 });
+	});
+
 });
