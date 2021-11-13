8fc8e8084e51a5ab6d90b2f5327b392968519c02
Andres Fernando <andres.fernando08@gmail.com>
2021-11-13 17:21:18 -0500

    [add] loop in tests

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -15,4 +15,12 @@ describe('test sum function', () => {
 		expect(data).toEqual({ one: 1, two: 2 });
 	});
 
+	test('adding positive numbers is not zero', () => {
+		for (let a = 1; a < 10; a++) {
+			for (let b = 1; b < 10; b++) {
+				expect(a + b).not.toBe(0);
+			}
+		}
+	});
+	
 });
